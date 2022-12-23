17f627118b55c15d261b3eae3969db3637a14349
Ubuntu <ubuntu@ip-172-31-20-235.eu-west-1.compute.internal>
2022-12-23 12:51:40 +0000

    modulo-claves

--- despliegues/infra-aws/main.tf
+++ despliegues/infra-aws/main.tf
@@ -1,10 +1,50 @@
+#locals {
+#    ami_a_usar = length(data.aws_instance.mimaquina) == 1 && ! var.force_upgrade ? data.aws_instance.mimaquina[0].ami : data.aws_ami.miami
+#}
+
 resource "aws_instance" "mimaquina" {
-    ami             = data.aws_ami.miami.image_id #"ami-00463ddd1036a8eb6" #### ESTO es ruina!
+    ami             = data.aws_ami.miami.image_id #"ami-00463ddd1036a8eb6" #### ESTO es ruina!   #local.ami_a_usar
     instance_type   = "t2.micro"
+    key_name        = aws_key_pair.miclave.key_name # NOMBRE de unas claves (solo la publica... openssh) registradas en amazon aws
+                      # ESTO DE AQUI ^^^ VA A FUNCIONAR ???? 
+                        # SI    I
+                        # NO    
+                        # NPI   IIIIII              < Ivan
+                      # Podría funcionar o no... e Ivan no lo tiene claro. Ya que... de que depende que esto vaya a funcionar? 
+                      # De qué recurso se haya ejecutado antes !
     tags = {
         Name        = "maquina-de-${var.nombre_despliegue}"
     }
+    # depends_on = [ aws_key_pair.miclave ] # ESTO ES UNA MIERDA GIGANTE !!!! y de paso una MUY MUY MUY MUY MALA PRACTICA !
+                                        # He de evitar totalmente usar esto. Y negaré haberoslo contado.
+                                        # En muy muy muy pocos caso necesito hacer algo como esto
+                                        # Y este no es un o de ellos.
+
+    
+    
 }
+
+resource "aws_key_pair" "miclave" {
+  key_name   = "clave-publica-de-${var.nombre_despliegue}"
+  public_key = module.misclaves.keys.publicKey.openssh # Meter la clave publica asociada a una clave privada mia en formato openssh
+}
+
+module "misclaves" {
+    source = "../modulo-claves"
+    
+    keys_path = "./claves/"
+    algorithm = {
+        name = "RSA"
+        config = "4096"
+    }
+    #force_generate = false
+}
+#data "aws_instance" "mimaquina" {
+#  filter {
+#    name   = "tag:Name"
+#    values = ["maquina-de-${var.nombre_despliegue}"]
+#  }
+#}
 
 data "aws_ami" "miami" {
   most_recent      = true
